15632b7933ce8031ee43f8a1d86beea25074c9ab
Wei Ning <nwcgnw@gmail.com>
2018-02-22 13:35:55 +1300

    - add multiple data sets example (index keyword)

---
+++ src/graph/multiple_datasets.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+function createMultiDatasetFile() {
+    echo "
+# Hits
+100
+101
+102
+
+
+70
+71
+72
+" > /tmp/_
+}
+
+function runGnuplot() {
+    echo "
+set key outside top center
+plot '/tmp/_' index 0 using 1 t 'dataset index 0' w lp pt 7,\
+'' index 1 using 1 t 'dataset index 1' w lp pt 7
+" > /tmp/_.gnuplot
+    gnuplot -p -c /tmp/_.gnuplot
+}
+
+function run() {
+    createMultiDatasetFile
+    runGnuplot
+}
+
+run
