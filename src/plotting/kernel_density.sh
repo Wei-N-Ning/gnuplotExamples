e51c625a07350948c38f964047d6457cec26473b
Wei Ning <nwcgnw@gmail.com>
2018-02-22 16:43:13 +1300

    - update plotting/kernel_density.sh, using internal variable STATS_records instead of hardcoded record count
    - update plotting/uniqueness.sh, using dynamic point size

--- src/plotting/kernel_density.sh
+++ src/plotting/kernel_density.sh
@@ -8,12 +8,15 @@ for i in xrange(50):
 " > /tmp/_
 }
 
+# see Gnuplot in Action 2nd, P96 (125) about internal variable
+# STATS_records
 function runGnuplot() {
     echo "
 set key outside center top title 'Data' box 3
-plot '/tmp/_' u 1:(-0.1) t 'Raw data' w p pt 7,\
-'' u 1:(1/50.0) t 'Kernel density 0.05' s kdens bandwidth 0.05,\
-'' u 1:(1/50.0) t 'Kernel density 0.1' s kdens bandwidth 0.1
+stats '/tmp/_'
+plot '' u 1:(-0.1) t 'Raw data' w p pt 7,\
+'' u 1:(1.0/STATS_records) t 'Kernel density 0.05' s kdens bandwidth 0.05,\
+'' u 1:(1.0/STATS_records) t 'Kernel density 0.1' s kdens bandwidth 0.1
 " > /tmp/_.gnuplot
     gnuplot -p -c /tmp/_.gnuplot
 }
--- src/plotting/uniqueness.sh
+++ src/plotting/uniqueness.sh
@@ -8,13 +8,17 @@ for i in xrange(200):
 " > /tmp/_
 }
 
+# this also shows how set dynamic point size 
+# note the extra column "1:2:2" and "ps variable"
+# see: http://gnuplot.sourceforge.net/demo/pointsize.html
+# see: https://stackoverflow.com/questions/2689403/gnuplot-dot-plot-with-data-depending-dot-size
 function runGnuplot() {
     echo "
 set key outside center top
 set xlabel 'Unique groups'
 set ylabel 'Average per group'
-plot [][-2:2] '/tmp/_' using 1:2 t 'Raw data' w p pt 7 ps 0.5,\
-'' using 1:2 smooth unique w p pt 7 ps 1.5
+plot [][-2:2] '/tmp/_' using 1:2:2 t 'Raw data' w p pt 6 ps variable,\
+'' using 1:2 t 'Average' smooth unique w p lt 1 pt 7 ps 1.5
 " > /tmp/_.gnuplot
     gnuplot -p -c /tmp/_.gnuplot
 }
