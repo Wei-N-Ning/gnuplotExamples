b29efcbf3f6b3d8d69c9174a57b0423cfd7ba69a
Wei Ning <nwcgnw@gmail.com>
2018-02-22 17:40:52 +1300

    - update hello_world/common_bigo.gnuplot script, changing the output to png and setting image size (this is useful in batch mode)

--- src/hello_world/common_bigo.gnuplot
+++ src/hello_world/common_bigo.gnuplot
@@ -1,5 +1,9 @@
 # plot the common bigo time complexity functions
 # beware that gnuplot use log10() for the logarithm to base 10
+set title "Visualization of time complexity O(n) of common algorithms"
+set key outside bottom center
+set terminal pngcairo size 1024,768
+set output "/tmp/out.png"
 unit=1#0.012
 plot [0.1:100][0.1:60] 10 * unit title "Constant", \
 log10(x * unit) title "Log(n)", \
--- src/plotting/simple_gallery.sh
+++ src/plotting/simple_gallery.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # the second call use abbreviations
+# note also the use of linecolor modifier
 function runGnuplot() {
     gnuplot -p -e "
 plot [-2.5:2.5][-2.5:2.5] 
 0.3 * sin(x) + 2 title 'linespoints' with linespoints,
 0.3 * sin(x) + 1.5 title 'default',
-0.3 * sin(x) + 1 title 'linesp' with linesp,
+0.3 * sin(x) + 1 title 'linesp' with linesp linecolor '0xFF8800',
 0.3 * sin(x) + 0.5 title 'points' with points,
 0.25 * x - 0.25 title 'boxes' with boxes
 "
