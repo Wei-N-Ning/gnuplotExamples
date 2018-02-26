88b597192a45437b949a3873a56f3472eee44feb
Wei Ning <nwcgnw@gmail.com>
2018-02-26 14:29:05 +1300

    - update hello_world/complexity_scale.sh, adding write-to-png functionality (pass in a file path)

--- src/hello_world/complexity_scale.sh
+++ src/hello_world/complexity_scale.sh
@@ -22,20 +22,47 @@ for size in xrange(100, 10000, 100):
 }
 
 function runGnuplot() {
-    echo '
+    echo "
 unset xtics
 unset ytics
 set size square
-set xlabel "Workload"
-set ylabel "Run time [sec]"
-plot "/tmp/complexity" using 1:2 title "Tim" with lines, "/tmp/complexity" using 1:3 title "Dumb" with lines
-' > /tmp/_.gnuplot
+set xlabel 'Workload'
+set ylabel 'Run time [sec]'
+plot '/tmp/complexity' using 1:2 title 'Tim' with lines, '/tmp/complexity' using 1:3 title 'Dumb' with lines
+" > /tmp/_.gnuplot
     gnuplot -p /tmp/_.gnuplot
+}
+
+function runGnuplotPNG() {
+    echo "
+unset xtics
+unset ytics
+set size square
+set xlabel 'Workload'
+set ylabel 'Run time [sec]'
+set terminal pngcairo
+set output '$1'
+plot '/tmp/complexity' using 1:2 title 'Tim' with lines, '/tmp/complexity' using 1:3 title 'Dumb' with lines
+" > /tmp/_.gnuplot
+    gnuplot -p /tmp/_.gnuplot
+}
+
+function canWriteToPNG() {
+    if [ ! -z "$1" ]; then
+        if [ -d `dirname "$1"` ]; then
+            return 0
+        fi
+    fi
+    return 1
 }
 
 function run() {
     runPythonExec
-    runGnuplot
+    if (canWriteToPNG $1); then
+        runGnuplotPNG $1
+    else 
+        runGnuplot
+    fi
 }
 
-run
+run $1
